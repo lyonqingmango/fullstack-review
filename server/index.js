6fcfb03a6259851e9bc7bac0b54d23b8f7ac051a
Joanna Zhang <31421459+lyonqingmango@users.noreply.github.com>
2022-06-17 15:15:47 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,17 +3,9 @@ let app = express();
 const bodyParser = require('body-parser');
 const {getReposByUsername} = require('../helpers/github.js');
 const {save, selectRepos} = require('../database/index.js');
-
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
-
-
-
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
   console.log('req.body'+JSON.stringify(req.body.username));
   console.log('req.body without'+req.body.username);
 
@@ -27,7 +19,6 @@ app.post('/repos', function (req, res) {
   }}))
   .catch((err)=>{console.log('err inside post');res.status(500).send(err)})
 });
-
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
